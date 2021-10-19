c663808ebfaa5d1487fd41174eddbb85e127f95b
XranaDev <79064535+XranaDev@users.noreply.github.com>
2021-10-19 13:03:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,37 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-client.on('Ready!',() => {
- console.log(`Logged in as {$client.user.tag}!`);
-});
+client.on('message', async msg => {
+  switch (msg.content) {
+    case "ping":
+      msg.reply("Pong!");
+      break;
+    //our meme command below
+    case "!meme":
+      msg.channel.send("Here's your meme!"); //Replies to user command
+      const img = await getMeme(); //fetches an URL from the API
+      msg.channel.send(img); //send the image URL
+      break;
+   }
+})
+
+let interval;
+
+client.on('message', async msg => {
+  switch (msg.content) {
+   //other commands above here...
+   case "!annoyme":
+      msg.channel.send("You are now annoying xrana!");
+       interval = setInterval (function () {
+        msg.channel.send("@xrana#9509")
+        .catch(console.error); 
+      }, 3600000); //every hour
+      break;
+     case "!stop":
+      msg.channel.send("I have stopped eye reminders.");
+      clearInterval(interval);
+      break;
+  }
+})
 
 client.login(process.env.TOKEN);
